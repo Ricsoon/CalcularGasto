45cfe6ee4974f9b624054fe301f506b23e635a15
Ricsoon <painonex2@gmail.com>
2022-08-01 14:27:04 -0300

    Adicionado calculo após o evento de click

--- app/src/main/java/com/horizoncorp/calculargasto/MainActivity.kt
+++ app/src/main/java/com/horizoncorp/calculargasto/MainActivity.kt
@@ -2,9 +2,11 @@ package com.horizoncorp.calculargasto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.horizoncorp.calculargasto.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -12,9 +14,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.buttonCalculate.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_calculate){
+            calculate()
+        }
     }
 
     private fun calculate(){
 
+        val distance = binding.editDistance.text.toString().toFloat()
+        val price = binding.editPrice.text.toString().toFloat()
+        val autonomy = binding.editAutonomy.text.toString().toFloat()
+
+        val totalValue = (distance * price) / autonomy
+
+        binding.textTotal.text = "R$ ${"%.2f".format(totalValue)}"
+
+        // Toast.makeText(this, totalValueStr, Toast.LENGTH_SHORT).show()
     }
 }
